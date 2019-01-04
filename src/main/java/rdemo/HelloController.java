22514a9da7ee567782fcd29ce61086a500433ed5
Jasper Lelijveld <lelijveldj@hotmail.com>
2019-01-04 13:17:27 +0100

    Update HelloController.java

--- src/main/java/rdemo/HelloController.java
+++ src/main/java/rdemo/HelloController.java
@@ -3,12 +3,13 @@ package rdemo;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+// return "Greetings from Spring Boot!"; verandert in NULL
 @RestController
 public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return Null;
     }
     
 }
